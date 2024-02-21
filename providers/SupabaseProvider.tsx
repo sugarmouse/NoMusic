1a86727b12c69bc3fb68fe53de5dcc86ca2b939f
sugarmouse <tanghao19950911@gmail.com>
2024-02-21 12:02:48 +0800

    add SupabaseProvider

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+
+import { Database } from "@/types_db";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({ children }) => {
+  const [supabaseClient] = useState(() => createPagesBrowserClient<Database>());
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+};
+
+export default SupabaseProvider;
